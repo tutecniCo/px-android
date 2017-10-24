d56b1579ed7ab7695643eed9dbf180cc31808225
matiasromar <matias.romar@mercadolibre>
2017-10-24 17:10:57 -0300

    added model objects

--- px-services/src/main/java/com/mercadopago/model/CardToken.java
+++ px-services/src/main/java/com/mercadopago/model/CardToken.java
@@ -1,19 +1,14 @@
 package com.mercadopago.model;
 
-import android.content.Context;
-import android.text.TextUtils;
-
-import com.mercadopago.exceptions.CardTokenException;
-import com.mercadopago.util.MercadoPagoUtil;
-
 import java.util.Calendar;
-import java.util.Locale;
+
+/**
+ * Created by mromar on 10/24/17.
+ */
 
 public class CardToken {
 
     private final static Calendar now = Calendar.getInstance();
-    public static final int MIN_LENGTH_NUMBER = 10;
-    public static final int MAX_LENGTH_NUMBER = 19;
 
     private Cardholder cardholder;
     private String cardNumber;
@@ -22,18 +17,8 @@ public class CardToken {
     private Integer expirationYear;
     private String securityCode;
 
-    public CardToken(String cardNumber, Integer expirationMonth, Integer expirationYear,
-                     String securityCode, String cardholderName, String identificationType, String identificationNumber) {
-        this.cardNumber = normalizeCardNumber(cardNumber);
-        this.expirationMonth = expirationMonth;
-        this.expirationYear = normalizeYear(expirationYear);
-        this.securityCode = securityCode;
-        this.cardholder = new Cardholder();
-        this.cardholder.setName(cardholderName);
-        Identification identification = new Identification();
-        identification.setNumber(identificationNumber);
-        identification.setType(identificationType);
-        this.cardholder.setIdentification(identification);
+    public static Calendar getNow() {
+        return now;
     }
 
     public Cardholder getCardholder() {
@@ -56,8 +41,8 @@ public class CardToken {
         return device;
     }
 
-    public void setDevice(Context context) {
-        this.device = new Device(context);
+    public void setDevice(Device device) {
+        this.device = device;
     }
 
     public Integer getExpirationMonth() {
@@ -73,7 +58,7 @@ public class CardToken {
     }
 
     public void setExpirationYear(Integer expirationYear) {
-        this.expirationYear = CardToken.normalizeYear(expirationYear);
+        this.expirationYear = expirationYear;
     }
 
     public String getSecurityCode() {
@@ -82,193 +67,5 @@ public class CardToken {
 
     public void setSecurityCode(String securityCode) {
         this.securityCode = securityCode;
-    }
-
-    public boolean validate(boolean includeSecurityCode) {
-        boolean result = validateCardNumber() && validateExpiryDate() && validateIdentification() && validateCardholderName();
-        if (includeSecurityCode) {
-            result = result && validateSecurityCode();
-        }
-        return result;
-    }
-
-    public boolean validateCardNumber() {
-        return !TextUtils.isEmpty(cardNumber) && (cardNumber.length() > MIN_LENGTH_NUMBER) && (cardNumber.length() < MAX_LENGTH_NUMBER);
-    }
-
-    public void validateCardNumber(PaymentMethod paymentMethod) throws CardTokenException {
-
-        // Empty field
-
-        if (cardNumber == null || cardNumber.isEmpty()) {
-            throw new CardTokenException(CardTokenException.INVALID_EMPTY_CARD);
-        }
-
-        Setting setting = Setting.getSettingByBin(paymentMethod.getSettings(), (cardNumber.length()
-                >= MercadoPagoUtil.BIN_LENGTH ? cardNumber.substring(0, MercadoPagoUtil.BIN_LENGTH) : ""));
-
-        if (setting == null) {
-
-            // Invalid bin
-            throw new CardTokenException(CardTokenException.INVALID_CARD_BIN);
-
-        } else {
-
-            // Validate cards length
-            int cardLength = setting.getCardNumber().getLength();
-            if (cardNumber.trim().length() != cardLength) {
-
-                throw new CardTokenException(CardTokenException.INVALID_CARD_LENGTH, String.valueOf(cardLength));
-            }
-
-            // Validate luhn
-            String luhnAlgorithm = setting.getCardNumber().getValidation();
-            if (("standard".equals(luhnAlgorithm)) && (!checkLuhn(cardNumber))) {
-                throw new CardTokenException(CardTokenException.INVALID_CARD_LUHN);
-            }
-        }
-    }
-
-    public boolean validateSecurityCode() {
-
-        return validateSecurityCode(securityCode);
-    }
-
-    public static boolean validateSecurityCode(String securityCode) {
-
-        return securityCode == null || (!TextUtils.isEmpty(securityCode) && securityCode.length() >= 3 && securityCode.length() <= 4);
-    }
-
-    public void validateSecurityCode(PaymentMethod paymentMethod) throws CardTokenException {
-
-        validateSecurityCode(securityCode, paymentMethod, (((cardNumber != null) ? cardNumber.length() : 0) >= MercadoPagoUtil.BIN_LENGTH ? cardNumber.substring(0, MercadoPagoUtil.BIN_LENGTH) : ""));
-    }
-
-    public static void validateSecurityCode(String securityCode, PaymentMethod paymentMethod, String bin) throws CardTokenException {
-
-        if (paymentMethod != null) {
-            Setting setting = Setting.getSettingByBin(paymentMethod.getSettings(), bin);
-
-            // Validate security code length
-            if (setting != null) {
-                int cvvLength = setting.getSecurityCode().getLength();
-                if ((securityCode == null) || ((cvvLength != 0) && (securityCode.trim().length() != cvvLength))) {
-                    throw new CardTokenException(CardTokenException.INVALID_CVV_LENGTH, String.valueOf(cvvLength));
-                }
-            } else {
-                throw new CardTokenException(CardTokenException.INVALID_FIELD);
-            }
-        }
-    }
-
-    public boolean validateExpiryDate() {
-
-        return validateExpiryDate(expirationMonth, expirationYear);
-    }
-
-    public static boolean validateExpiryDate(Integer month, Integer year) {
-
-        if (!validateExpMonth(month)) {
-            return false;
-        }
-        if (!validateExpYear(year)) {
-            return false;
-        }
-        return !hasMonthPassed(month, year);
-    }
-
-    public static boolean validateExpMonth(Integer month) {
-
-        return (month == null) ? false : (month >= 1 && month <= 12);
-    }
-
-    public static boolean validateExpYear(Integer year) {
-
-        return (year == null) ? false : !hasYearPassed(year);
-    }
-
-    public boolean validateIdentification() {
-        return validateIdentificationType() && validateIdentificationNumber();
-    }
-
-    public boolean validateIdentificationType() {
-        return (cardholder == null) ? false :
-                (cardholder.getIdentification() == null) ? false :
-                        !TextUtils.isEmpty(cardholder.getIdentification().getType());
-    }
-
-    public boolean validateIdentificationNumber() {
-        return (cardholder == null) ? false :
-                (cardholder.getIdentification() == null) ? false :
-                        (!validateIdentificationType()) ? false :
-                                !TextUtils.isEmpty(cardholder.getIdentification().getNumber());
-    }
-
-    public boolean validateIdentificationNumber(IdentificationType identificationType) {
-        if (identificationType != null) {
-            if ((cardholder != null) &&
-                    (cardholder.getIdentification() != null) &&
-                    (cardholder.getIdentification().getNumber() != null)) {
-                int len = cardholder.getIdentification().getNumber().length();
-                Integer min = identificationType.getMinLength();
-                Integer max = identificationType.getMaxLength();
-                if ((min != null) && (max != null)) {
-                    return ((len <= max) && (len >= min));
-                } else {
-                    return validateIdentificationNumber();
-                }
-            } else {
-                return false;
-            }
-        } else {
-            return validateIdentificationNumber();
-        }
-    }
-
-    public boolean validateCardholderName() {
-        return cardholder != null && cardholder.getName() != null && !cardholder.getName().isEmpty();
-    }
-
-    public static boolean checkLuhn(String cardNumber) {
-
-        int sum = 0;
-        boolean alternate = false;
-        if ((cardNumber == null) || (cardNumber.length() == 0)) {
-            return false;
-        }
-        for (int i = cardNumber.length() - 1; i >= 0; i--) {
-            int n = Integer.parseInt(cardNumber.substring(i, i + 1));
-            if (alternate) {
-                n *= 2;
-                if (n > 9) {
-                    n = (n % 10) + 1;
-                }
-            }
-            sum += n;
-            alternate = !alternate;
-        }
-        return (sum % 10 == 0);
-    }
-
-    private static boolean hasYearPassed(int year) {
-        int normalized = normalizeYear(year);
-        return normalized < now.get(Calendar.YEAR);
-    }
-
-    private static boolean hasMonthPassed(int month, int year) {
-        return hasYearPassed(year) || normalizeYear(year) == now.get(Calendar.YEAR) && month < (now.get(Calendar.MONTH) + 1);
-    }
-
-    private static Integer normalizeYear(Integer year) {
-        if ((year != null) && (year < 100 && year >= 0)) {
-            String currentYear = String.valueOf(now.get(Calendar.YEAR));
-            String prefix = currentYear.substring(0, currentYear.length() - 2);
-            year = Integer.parseInt(String.format(Locale.US, "%s%02d", prefix, year));
-        }
-        return year;
-    }
-
-    private String normalizeCardNumber(String number) {
-        return number.trim().replaceAll("\\s+|-", "");
     }
 }
