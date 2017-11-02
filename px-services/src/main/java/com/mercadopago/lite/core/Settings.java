7030291228efe3b14c57ab078b3007f29b7b2e6f
matiasromar <matias.romar@mercadolibre>
2017-11-02 11:11:17 -0300

    deleted lite model lib

--- px-services/src/main/java/com/mercadopago/lite/core/Settings.java
+++
@@ -1,7 +0,0 @@
-package com.mercadopago.lite.core;
-
-import okhttp3.logging.HttpLoggingInterceptor;
-
-public class Settings {
-    public static final HttpLoggingInterceptor.Level OKHTTP_LOGGING = HttpLoggingInterceptor.Level.BODY;
-}
