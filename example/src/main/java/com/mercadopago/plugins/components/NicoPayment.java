0518023fd7e75fc75d022e53f7643a318d7cce60
Nicolas Fortunato <nicolas.fortunato@mercadolibre.com>
2017-12-29 10:54:43 -0300

    PluginPaymentResult added, result screen fizes

--- example/src/main/java/com/mercadopago/plugins/components/NicoPayment.java
+++ example/src/main/java/com/mercadopago/plugins/components/NicoPayment.java
@@ -3,16 +3,13 @@ package com.mercadopago.plugins.components;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 
-import com.mercadopago.plugins.PluginComponent;
 import com.mercadopago.components.RendererFactory;
-import com.mercadopago.constants.PaymentTypes;
 import com.mercadopago.core.CheckoutStore;
 import com.mercadopago.model.Payment;
-import com.mercadopago.model.PaymentData;
-import com.mercadopago.model.PaymentMethod;
-import com.mercadopago.model.PaymentResult;
-import com.mercadopago.plugins.PaymentResultAction;
+import com.mercadopago.plugins.PluginComponent;
+import com.mercadopago.plugins.PluginPaymentResultAction;
 import com.mercadopago.plugins.model.PaymentMethodInfo;
+import com.mercadopago.plugins.model.PluginPaymentResult;
 
 /**
  * Created by nfortuna on 12/13/17.
@@ -25,8 +22,6 @@ public class NicoPayment extends PluginComponent {
     static {
         RendererFactory.register(NicoPayment.class, NicoPaymentRenderer.class);
     }
-
-    private String document;
 
     public NicoPayment(@NonNull final Props props) {
         super(props);
@@ -42,39 +37,15 @@ public class NicoPayment extends PluginComponent {
                 final PaymentMethodInfo paymentMethodInfo =
                         CheckoutStore.getInstance().getSelectedPaymentMethod();
 
-                final Payment payment = new Payment();
-                payment.setId(Long.parseLong("123456"));
-                payment.setMetadata(null);
-                payment.setPaymentMethodId(paymentMethodInfo.id);
-                payment.setPaymentTypeId(PaymentTypes.PLUGIN);
-                payment.setPaymentTypeId(PaymentTypes.PLUGIN);
+                final PluginPaymentResult result = new PluginPaymentResult(
+                        123456l,
+                        Payment.StatusCodes.STATUS_REJECTED,
+                        Payment.StatusCodes.STATUS_DETAIL_CC_REJECTED_OTHER_REASON,
+                        paymentMethodInfo);
 
-                payment.setStatus(Payment.StatusCodes.STATUS_REJECTED);
-                payment.setStatusDetail(Payment.StatusCodes.STATUS_DETAIL_CC_REJECTED_OTHER_REASON);
-
-                final PaymentData paymentData = new PaymentData();
-                final PaymentMethod paymentMethod = new PaymentMethod();
-                paymentMethod.setId(paymentMethodInfo.id);
-                paymentMethod.setName(paymentMethodInfo.name);
-                paymentMethod.setPaymentTypeId(PaymentTypes.PLUGIN);
-                paymentData.setPaymentMethod(paymentMethod);
-
-                onPaymentResult(createPaymentResult(payment, paymentData));
+                getDispatcher().dispatch(new PluginPaymentResultAction(result));
             }
         }, 2500);
-    }
-
-    public void onPaymentResult(@NonNull final PaymentResult result) {
-        getDispatcher().dispatch(new PaymentResultAction(result));
-    }
-
-    private PaymentResult createPaymentResult(final Payment payment, final PaymentData paymentData) {
-        return new PaymentResult.Builder()
-            .setPaymentData(paymentData)
-            .setPaymentId(payment.getId())
-            .setPaymentStatus(payment.getStatus())
-            .setPaymentStatusDetail(payment.getStatusDetail())
-            .build();
     }
 
     public String getDocument() {
